0773765f156e88a3db7f77f1d2a5bc4f240d241d
Ulul Fadhli <ulfdh244@gmail.com>
2024-02-09 15:00:41 +0700

    jawaban yang benar setelah di koreksi Mas Abu

--- tugasMethod/3.js
+++ tugasMethod/3.js
@@ -1,7 +1,8 @@
 let numbers = [1, 2, 3, 4, 5]
 
 let numbers2 = numbers.map(value => {
-  return value * value * value
+  // return value * value * value
+  return value ** 5
 })
 
 console.log(numbers2);
