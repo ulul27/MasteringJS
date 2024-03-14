0244edb86fdf084ef9be848bb684f833d6d85b0a
Ulul Fadhli <ulfdh244@gmail.com>
2024-03-15 06:55:17 +0700

    di rubah supaya inget-ingetnya mudah

---
+++ pertemuan-011/001-lat05-boxmodel-membuatwebsite/dom.js
@@ -0,0 +1,7 @@
+// let judul = document.getElementsByClassName('judul')[0]
+// judul.style.color = 'red'
+
+document.getElementsByClassName('judul')[0].style.color = 'green'
+
+
+console.log(judul);
